b21694bd3f6a886667f3a6c3b53ba083c4c4921f
Jeni Dang <jenipdang@yahoo.com>
2022-02-18 10:41:12 -0800

    commit tree 3109918fd415c6fd74da819c9fe5774321482308

--- ruby/find_first_duplicate.rb
+++ ruby/find_first_duplicate.rb
@@ -1,6 +1,14 @@
+require 'pry'
 def find_first_duplicate(arr)
   # type your code in here
+  duplicate = []
+  arr.each do |i|
+    return i if duplicate[i]
+    duplicate[i] = true
+  end
+  -1
 end
+binding.pry
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 3"
@@ -15,4 +23,9 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+## return the first duplicate
+## a == b, b == c return b
+## return -1 if no duplicate
+
+
 # And a written explanation of your solution
